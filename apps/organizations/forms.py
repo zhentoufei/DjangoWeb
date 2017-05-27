5b18dbe2c06a5156967e68e4eff6d33f5cd60ea5
zhentoufei <zhentoufei@163.com>
2017-05-27 22:36:09 +0800

    完成了UserAskForms的设计

--- apps/organizations/forms.py
+++ apps/organizations/forms.py
@@ -3,6 +3,7 @@ __author__ = 'Mr.Finger'
 __date__ = '2017/5/27 20:40'
 from django import forms
 from operation.models import UserAsk
+import re
 
 # class UserAskForm(forms.Form):
 #     name = forms.CharField(required=True, min_length=2, max_length=20)
@@ -16,3 +17,16 @@ class UserAskForm(forms.ModelForm):
     class Meta:
         model = UserAsk
         fields = ['name', 'mobile', 'course_name']
+
+    def clean_mobile(self):
+        '''
+        验证手机号码是否合法
+        :return: 
+        '''
+        mobile = self.cleaned_data["mobile"]
+        REGEX_MOBILE = R"^1(3[0-9]|4[57]|5[0-35-9]|7[0135678]|8[0-9])\d{8}$"
+        p = re.compile(REGEX_MOBILE)
+        if p.match(mobile):
+            return mobile
+        else:
+            raise forms.ValidationError(u"手机号码非法", code="mobile_invalid")
