96bb8803054c5249a7c8698f56d78ec5cc6d8ea1
zhentoufei <zhentoufei@163.com>
2017-05-27 22:07:15 +0800

    修改了由于json格式错误引起的UserAsk不执行的bug

--- apps/organizations/urls.py
+++ apps/organizations/urls.py
@@ -3,9 +3,9 @@ __author__ = 'Mr.Finger'
 __date__ = '2017/5/27 20:49'
 
 from django.conf.urls import url, include
-from .views import OrgView
+from .views import OrgView, AddUserAskView
 urlpatterns = [
     #课程机构列表页
     url(r'^list/$', OrgView.as_view(), name='org_list'),
-    url(r'^add_ask/$', OrgView.as_view(), name='org_list'),
+    url(r'^add_ask/$', AddUserAskView.as_view(), name='add_ask'),
 ]
--- apps/organizations/views.py
+++ apps/organizations/views.py
@@ -1,10 +1,12 @@
 # -*- coding:utf-8 -*-
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpResponse
 # Create your views here.
 from .models import CourseOrg, CityDict
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from .forms import UserAskForm
+
 
 class OrgView(View):
     """
@@ -55,7 +57,13 @@ class OrgView(View):
 
 
 class AddUserAskView(View):
+    '''
+    用户添加咨询
+    '''
     def post(self, request):
         userask_form = UserAskForm(request.POST)
         if userask_form.is_valid():
             user_ask = userask_form.save(commit=True)
+            return HttpResponse('{"status":"success"}', content_type='application/json')
+        else:
+            return HttpResponse('{"status":"fail", "msg":"添加出错"}', content_type='application/json')
