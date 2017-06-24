f953d94a646aead54b01c5aab25af79075a1299d
zhentoufei <zhentoufei@163.com>
2017-06-24 22:34:31 +0800

    course的model更新

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from datetime import datetime
 
 from django.db import models
-from organizations.models import CourseOrg
+from organizations.models import CourseOrg,Teacher
 # Create your models here.
 
 
@@ -13,11 +13,16 @@ class Course(models.Model):
     desc = models.CharField(max_length=300, verbose_name=u"课程描述")
     detail = models.TextField(verbose_name=u"课程详情")
     degree = models.CharField(choices=(("cj", u"初级"), ("zj", u"中级"), ("gj", u"高级")), max_length=2, verbose_name=u"难度")
+    teacher = models.ForeignKey(Teacher, verbose_name=u'授课讲师', null=True, blank=True)
     learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟)")
     students = models.IntegerField(default=0, verbose_name=u"学习人数")
     fav_nums = models.IntegerField(default=0, verbose_name=u"收藏人数")
     image = models.ImageField(upload_to="courses/%Y/%m", verbose_name=u"封面")
     click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
+    category = models.CharField(max_length=30, verbose_name=u'课程类别', default=u'后端开发')
+    tag = models.CharField(default="", verbose_name=u'课程标签', max_length=10)
+    you_need_know = models.CharField(max_length=30, verbose_name=u'课程须知', default=u'课程须知')
+    teacher_tell = models.CharField(max_length=30, verbose_name=u'老师告诉你', default=u'老师告诉你')
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
     class Meta:
@@ -27,25 +32,47 @@ class Course(models.Model):
     def __unicode__(self):
         return self.name
 
+    #获取课程章节数目
+    def get_zj_nums(self):
+        return self.lesson_set.all().count()
+
+    def get_learn_users(self):
+        return self.usercourse_set.all()[:5]
+
+    def get_course_lesson(self):
+        #获取课程所有章节
+        return self.lesson_set.all()
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, verbose_name=u"课程")
     name = models.CharField(max_length=100, verbose_name=u"章节")
+    learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟)")
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
     class Meta:
         verbose_name = u"章节"
         verbose_name_plural = verbose_name
 
+    def get_lesson_video(self):
+        #获取章节视频
+        return self.video_set.all()
+
+    def __unicode__(self):
+        return self.name
 
 class Video(models.Model):
     lesson = models.ForeignKey(Lesson, verbose_name=u"章节")
     name = models.CharField(max_length=100, verbose_name=u"视频名")
+    url = models.CharField(max_length=200, default="", verbose_name=u'访问地址')
+    learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟)")
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
     class Meta:
         verbose_name = u"视频"
         verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
 
 
 class CourseResource(models.Model):
