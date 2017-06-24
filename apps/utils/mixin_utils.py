eb628ade255b10aaed288597eae0153f6351f50d
zhentoufei <zhentoufei@163.com>
2017-06-24 22:39:52 +0800

    验证用户登录的一个父类

---
+++ apps/utils/mixin_utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Mr.Finger'
+__date__ = '2017/6/24 20:27'
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
+class LoginRequiredMixin(object):
+    @method_decorator(login_required(login_url='/login/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
